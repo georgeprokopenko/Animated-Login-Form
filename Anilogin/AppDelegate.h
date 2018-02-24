34018fa8d05cb82f0e64b963f5eccaccd791d38a
George Prokopenko <green.mobile@mail.ru>
2018-02-25 00:27:12 +0800

    Initial Commit

---
+++ Anilogin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Anilogin
+//
+//  Created by George Prokopenko on 25.02.2018.
+//  Copyright © 2018 George Prokopenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Anilogin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Anilogin
+//
+//  Created by George Prokopenko on 25.02.2018.
+//  Copyright © 2018 George Prokopenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
